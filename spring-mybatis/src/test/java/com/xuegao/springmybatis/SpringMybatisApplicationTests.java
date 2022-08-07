64f8666b5249f43649939222128fa905668e748f
xuegao <hubeixuegao@gmail.com>
2022-08-08 01:04:20 +0800

    spring-mybatis

--- spring-mybatis/src/test/java/com/xuegao/springmybatis/SpringMybatisApplicationTests.java
+++ spring-mybatis/src/test/java/com/xuegao/springmybatis/SpringMybatisApplicationTests.java
@@ -1,10 +1,10 @@
-package com.xuegao.xuegaospringotherues;
+package com.xuegao.springmybatis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class XueGaoSpringOtherUesApplicationTests {
+class SpringMybatisApplicationTests {
 
     @Test
     void contextLoads() {
