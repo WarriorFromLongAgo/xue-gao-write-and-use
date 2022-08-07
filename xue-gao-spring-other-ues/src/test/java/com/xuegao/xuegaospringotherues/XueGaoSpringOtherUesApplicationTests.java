4eda82f4892d04e729a5c6f621c3121f22053e42
xuegao <hubeixuegao@gmail.com>
2022-08-07 22:54:54 +0800

    common

---
+++ xue-gao-spring-other-ues/src/test/java/com/xuegao/xuegaospringotherues/XueGaoSpringOtherUesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.xuegaospringotherues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XueGaoSpringOtherUesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
