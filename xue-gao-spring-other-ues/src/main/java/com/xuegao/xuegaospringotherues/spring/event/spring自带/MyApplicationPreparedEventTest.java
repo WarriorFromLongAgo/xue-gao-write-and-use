87ac25914fdafaf3423bc2abdb9903d4cdb12730
xuegao <hubeixuegao@gmail.com>
2022-08-09 23:54:14 +0800

    event

--- xue-gao-spring-other-ues/src/main/java/com/xuegao/xuegaospringotherues/spring/event/spring自带/MyApplicationPreparedEventTest.java
+++ xue-gao-spring-other-ues/src/main/java/com/xuegao/xuegaospringotherues/spring/event/spring自带/MyApplicationPreparedEventTest.java
@@ -1,6 +1,6 @@
 package com.xuegao.xuegaospringotherues.spring.event.spring自带;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import lombok.Getter;
 import lombok.Setter;
 import org.slf4j.Logger;
