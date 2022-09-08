8b930dfd61b9a0654218a6e95d8c1c5b089aac59
xuegao <hubeixuegao@gmail.com>
2022-09-09 01:10:26 +0800

    batch

---
+++ xuegao-cloud-1/hr-service/src/main/java/com/xuegao/hr/HrServiceApplication.java
@@ -0,0 +1,9 @@
+// package com.xuegao.hr;
+//
+// @SpringBootApplication
+// public class HrServiceApplication {
+//     public static void main(String[] args) {
+//         SpringApplication.run(HrServiceApplication.class, args);
+//     }
+// }
+//
