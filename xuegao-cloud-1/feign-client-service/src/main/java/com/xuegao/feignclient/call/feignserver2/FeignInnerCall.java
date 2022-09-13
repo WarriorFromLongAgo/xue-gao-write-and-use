640b2d4d4d563edc68756e28abf00e48e04ed844
xuegao <hubeixuegao@gmail.com>
2022-09-13 23:46:47 +0800

    log 依赖报错问题解决了。sofa-trace-boot，

--- xuegao-cloud-1/feign-client-service/src/main/java/com/xuegao/feignclient/call/feignserver2/FeignInnerCall.java
+++ xuegao-cloud-1/feign-client-service/src/main/java/com/xuegao/feignclient/call/feignserver2/FeignInnerCall.java
@@ -1,26 +1,26 @@
-// package com.xuegao.feignclient.call.feignserver2;
-//
-// import com.xuegao.feignserver.business.service.InnerService;
-// import com.xuegao.model.vo.TestResponse;
-// import com.xuegao.util.JsonUtil;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Import;
-// import org.springframework.stereotype.Component;
-//
-// @Component
-// @Import(InnerService.class)
-// public class FeignInnerCall {
-//     private static final Logger log = LoggerFactory.getLogger(FeignInnerCall.class);
-//
-//     @Autowired
-//     private InnerService innerService;
-//
-//     public TestResponse test1() {
-//         TestResponse response = innerService.test1();
-//         log.info("[xue-gao-write-and-use][feignclient][FeignInnerCall][test1][={}]", JsonUtil.toJsonString(response));
-//         return response;
-//     }
-//
-// }
+package com.xuegao.feignclient.call.feignserver2;
+
+import com.xuegao.feignserver.business.service.InnerService;
+import com.xuegao.model.vo.TestResponse;
+import com.xuegao.util.JsonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
+
+@Component
+@Import(InnerService.class)
+public class FeignInnerCall {
+    private static final Logger log = LoggerFactory.getLogger(FeignInnerCall.class);
+
+    @Autowired
+    private InnerService innerService;
+
+    public TestResponse test1() {
+        TestResponse response = innerService.test1();
+        log.info("[xue-gao-write-and-use][feignclient][FeignInnerCall][test1][={}]", JsonUtil.toJsonString(response));
+        return response;
+    }
+
+}
--- xuegao-cloud-1/feign-client-service/src/main/java/com/xuegao/feignclient/controller/FeignInnerController.java
+++ xuegao-cloud-1/feign-client-service/src/main/java/com/xuegao/feignclient/controller/FeignInnerController.java
@@ -1,23 +1,23 @@
-// package com.xuegao.feignclient.controller;
-//
-// import com.xuegao.feignclient.call.feignserver2.FeignInnerCall;
-// import com.xuegao.model.vo.TestResponse;
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestMethod;
-// import org.springframework.web.bind.annotation.RestController;
-//
-// @RestController
-// public class FeignInnerController {
-//     private static final Logger log = LoggerFactory.getLogger(FeignInnerController.class);
-//
-//     @Autowired
-//     private FeignInnerCall feignInnerCall;
-//
-//     @RequestMapping(path = "/feignInner/test1", method = RequestMethod.GET)
-//     public TestResponse test1() {
-//         return feignInnerCall.test1();
-//     }
-// }
+package com.xuegao.feignclient.controller;
+
+import com.xuegao.feignclient.call.feignserver2.FeignInnerCall;
+import com.xuegao.model.vo.TestResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FeignInnerController {
+    private static final Logger log = LoggerFactory.getLogger(FeignInnerController.class);
+
+    @Autowired
+    private FeignInnerCall feignInnerCall;
+
+    @RequestMapping(path = "/feignInner/test1", method = RequestMethod.GET)
+    public TestResponse test1() {
+        return feignInnerCall.test1();
+    }
+}
