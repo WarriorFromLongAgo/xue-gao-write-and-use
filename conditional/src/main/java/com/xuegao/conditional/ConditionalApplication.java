96020afb61f5586254adf61780dc7d56802bf492
WarriorFromLongAgo <hubeixuegao@gmail.com>
2022-09-17 15:35:58 +0800

    use-jdk

---
+++ conditional/src/main/java/com/xuegao/conditional/ConditionalApplication.java
@@ -0,0 +1,13 @@
+package com.xuegao.conditional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConditionalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConditionalApplication.class, args);
+    }
+
+}
---
+++ conditional/src/test/java/com/xuegao/conditional/ConditionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuegao.conditional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
