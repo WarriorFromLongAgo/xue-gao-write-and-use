38251a322ee386158d8a2c77526798621bb11379
WarriorFromLongAgo <hubeixuegao@gmail.com>
2022-09-24 21:35:58 +0800

    agent

--- useagent1/src/main/java/com/xuegao/useagent1/controller/TestController.java
+++ useagent1/src/main/java/com/xuegao/useagent1/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.xuegao.useagent1.controller;
 
 import com.xuegao.agent.MyAgent;
+import com.xuegao.useagent1.model.TestDO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,9 @@ public class TestController {
 
     @RequestMapping("/test2")
     public String test2() {
-        long l = MyAgent.sizeOf("");
+        TestDO testDO = new TestDO();
+        long l = MyAgent.sizeOf(testDO);
+        System.out.println("testDO size = " + l);
         return "aklsjdkasjkad";
     }
 }
---
+++ useagent1/src/main/java/com/xuegao/useagent1/model/TestDO.java
@@ -0,0 +1,21 @@
+package com.xuegao.useagent1.model;
+
+// 默认就是32的大小
+public class TestDO {
+    // 8 _markword
+    // 4 _oop指针
+
+    // 4
+    // String str;
+
+    // 4
+    int i1;
+
+    // 4
+    // Object o;
+
+    // 1
+    // byte b1;
+    // byte b2;
+    // byte b3;
+}
