2af8567b578ab208bda3edad762f15df85ff1479
WarriorFromLongAgo <hubeixuegao@gmail.com>
2022-11-29 19:08:48 +0800

    AQS,stream

--- use-jdk/src/main/java/javatest/util/concurrent/locks/reentrantlock/Increment.java
+++ use-jdk/src/main/java/javatest/util/concurrent/locks/reentrantlock/Increment.java
@@ -15,6 +15,7 @@ public class Increment {
         try {
             TimeUnit.SECONDS.sleep(5);
             count++;
+            System.out.println(count);
         } catch (InterruptedException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -29,5 +30,6 @@ public class Increment {
         for (int i = 0; i < 100; i++) {
             new Thread(increment::increment).start();
         }
+
     }
 }
