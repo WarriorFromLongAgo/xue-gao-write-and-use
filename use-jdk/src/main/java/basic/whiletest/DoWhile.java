ecefac2c6206641562a7665d1364269ed955c0b2
feijiumeng <Xuegaoyu3>
2022-12-12 14:53:50 +0800

    do while

---
+++ use-jdk/src/main/java/basic/whiletest/DoWhile.java
@@ -0,0 +1,11 @@
+package basic.whiletest;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 10);
+    }
+}
