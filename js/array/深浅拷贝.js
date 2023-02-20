06e3c9560847b0b125b9347ba5408cbb9e4dd8d3
WarriorFromLongAgo <hubeixuegao@gmail.com>
2023-02-20 10:31:41 +0800

    ts

--- js/array/深浅拷贝.js
+++ js/array/深浅拷贝.js
@@ -3,3 +3,22 @@ const arr2 = arr.slice(0);
 arr[2] = 5;
 console.log(arr);
 console.log(arr2);
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+const rectangle1 = new Rectangle(1, 1);
+const rectangle2 = new Rectangle(2, 2);
+const rectangle3 = new Rectangle(3, 3);
+const rectangle4 = new Rectangle(4, 4);
+const rectangleArr1 = [rectangle1, rectangle2, rectangle3, rectangle4];
+console.log(rectangleArr1);
+const rectangleArr2 = rectangleArr1.slice(0);
+const tempRectangle2 = rectangleArr2[2];
+tempRectangle2.height = 2222;
+console.log(rectangleArr1);
+console.log(rectangleArr2);
