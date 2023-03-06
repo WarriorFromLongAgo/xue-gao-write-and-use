e781a036250606f8292119142569917cae7aae5b
feijiumeng <Xuegaoyu3>
2023-03-06 10:22:07 +0800

    do while

--- use-jdk/src/main/java/javatest/util/optional/optional/OfNullableTest.java
+++ use-jdk/src/main/java/javatest/util/optional/optional/OfNullableTest.java
@@ -1,10 +1,14 @@
 package javatest.util.optional.optional;
 
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
+import com.xuegao.util.JsonUtil;
+import common.model.temp.TempListBO;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 /**
  * Passing a non-null argument to 'Optional'
@@ -14,12 +18,19 @@ public class OfNullableTest {
         // test1();
 
         // test2();
-        List<String> nullList = null;
-        test2(nullList);
-        List<String> notNullList = new ArrayList<>();
-        test2(notNullList);
+        // List<String> nullList = null;
+        // test3(nullList);
+        // List<String> notNullList = new ArrayList<>();
+        // test3(notNullList);
+        // List<String> strList = Lists.newArrayList("1", "2");
+        // test3(strList);
 
-
+        List<TempListBO> nullList = null;
+        test3(nullList);
+        List<TempListBO> notNullList = new ArrayList<>();
+        test3(notNullList);
+        List<TempListBO> strList = TempListBO.getList();
+        test3(strList);
     }
 
     private static void test1() {
@@ -36,4 +47,16 @@ public class OfNullableTest {
         String listStr = Optional.ofNullable(inputList).map(JSON::toJSONString).orElse(null);
         System.out.println(listStr);
     }
+
+    private static void test3(List<TempListBO> tempListBOList) {
+        System.out.println("==============================================");
+        System.out.println("入参 = " + JsonUtil.toJsonString(tempListBOList));
+        String nullListStr = Optional.ofNullable(tempListBOList).map(JSON::toJSONString).orElse(null);
+        System.out.println("nullListStr = " + nullListStr);
+
+        String notNullListStr = Optional.ofNullable(tempListBOList)
+                .flatMap(c -> c.stream().findFirst()).map(JsonUtil::toJsonString).orElse(null);
+        System.out.println("notNullListStr = " + notNullListStr);
+    }
+
 }
