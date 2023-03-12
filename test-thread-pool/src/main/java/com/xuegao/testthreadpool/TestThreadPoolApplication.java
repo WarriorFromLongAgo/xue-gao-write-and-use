12b75358dd08f2ad87b80eea2c422e0252e9f4b0
WarriorFromLongAgo <hubeixuegao@gmail.com>
2023-03-12 22:37:12 +0800

    test

---
+++ test-thread-pool/src/main/java/com/xuegao/testthreadpool/TestThreadPoolApplication.java
@@ -0,0 +1,13 @@
+package com.xuegao.testthreadpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestThreadPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestThreadPoolApplication.class, args);
+    }
+
+}
