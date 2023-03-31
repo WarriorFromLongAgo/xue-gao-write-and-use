148ad5c96d9d4350917c4a22781c2baba48c7155
WarriorFromLongAgo <hubeixuegao@gmail.com>
2023-03-31 14:46:30 +0800

    open ai

---
+++ use-jdk/src/main/java/otherjar/Grt1228_chatgpt_java/Grt1228Constant.java
@@ -0,0 +1,7 @@
+package otherjar.Grt1228_chatgpt_java;
+
+public class Grt1228Constant {
+
+    public static final String API_KEY = "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";
+
+}
---
+++ use-jdk/src/main/java/otherjar/Grt1228_chatgpt_java/Test1.java
@@ -0,0 +1,30 @@
+package otherjar.Grt1228_chatgpt_java;
+
+import com.unfbx.chatgpt.OpenAiStreamClient;
+import com.unfbx.chatgpt.entity.chat.ChatCompletion;
+import com.unfbx.chatgpt.entity.chat.Message;
+import com.unfbx.chatgpt.sse.ConsoleEventSourceListener;
+
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+
+public class Test1 {
+    public static void main(String[] args) {
+        OpenAiStreamClient client = OpenAiStreamClient.builder()
+                .apiKey(Arrays.asList(Grt1228Constant.API_KEY))
+                //自己做了代理就传代理地址，没有可不不传
+//                .apiHost("https://自己代理的服务器地址/")
+                .build();
+        //聊天模型：gpt-3.5
+        ConsoleEventSourceListener eventSourceListener = new ConsoleEventSourceListener();
+        Message message = Message.builder().role(Message.Role.USER).content("你好啊我的伙伴！").build();
+        ChatCompletion chatCompletion = ChatCompletion.builder().messages(Arrays.asList(message)).build();
+        client.streamChatCompletion(chatCompletion, eventSourceListener);
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
