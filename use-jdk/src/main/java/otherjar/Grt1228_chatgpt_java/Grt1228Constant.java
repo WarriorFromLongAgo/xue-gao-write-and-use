8357eda18d27e20064e45c1a1b29ec6b9aa524f5
WarriorFromLongAgo <hubeixuegao@gmail.com>
2023-04-01 15:34:19 +0800

    open ai

--- use-jdk/src/main/java/otherjar/Grt1228_chatgpt_java/Grt1228Constant.java
+++ use-jdk/src/main/java/otherjar/Grt1228_chatgpt_java/Grt1228Constant.java
@@ -2,6 +2,6 @@ package otherjar.Grt1228_chatgpt_java;
 
 public class Grt1228Constant {
 
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "sk-";
 
 }
