38d26bdc145b7d77b641a5686146ff3e7a08a4b5
feijiumeng <feijiumeng@sweetnight.com>
2023-07-04 17:30:01 +0800

    substring

--- use-jdk/src/main/java/javatest/lang/string/SubStringTest.java
+++ use-jdk/src/main/java/javatest/lang/string/SubStringTest.java
@@ -5,5 +5,11 @@ public class SubStringTest {
         String str = "SN-ABB001-SK";
         String substring = str.substring(0, str.lastIndexOf("-"));
         System.out.println(substring);
+
+        String str2 = "24.6666";
+        String[] split = str2.split("\\.");
+        String substring1 = split[1].substring(0, 2);
+        System.out.println(substring1);
+
     }
 }
