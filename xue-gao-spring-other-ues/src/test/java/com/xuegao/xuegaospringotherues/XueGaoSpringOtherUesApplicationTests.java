b216e71aeccb2e3a5d66bfdfb159cc43fcafbd8e
xuegao <hubeixuegao@gmail.com>
2023-12-10 14:14:46 +0800

    redis

--- xue-gao-spring-other-ues/src/test/java/com/xuegao/xuegaospringotherues/XueGaoSpringOtherUesApplicationTests.java
+++ xue-gao-spring-other-ues/src/test/java/com/xuegao/xuegaospringotherues/XueGaoSpringOtherUesApplicationTests.java
@@ -1,13 +1,13 @@
 package com.xuegao.xuegaospringotherues;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XueGaoSpringOtherUesApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
--- xue-gao-spring-other-ues/src/test/java/com/xuegao/xuegaospringotherues/redissson/TestMap.java
+++ xue-gao-spring-other-ues/src/test/java/com/xuegao/xuegaospringotherues/redissson/TestMap.java
@@ -1,27 +1,30 @@
 package com.xuegao.xuegaospringotherues.redissson;
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.redisson.Redisson;
 import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootTest
 public class TestMap {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() throws InterruptedException {
         // 1. Create config object
         Config config = new Config();
-        config.useClusterServers()
-                // use "rediss://" for SSL connection
-                .addNodeAddress("redis://127.0.0.1:7181");
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
 
         RedissonClient redisson = Redisson.create(config);
         RMap<Object, Object> hashmap = redisson.getMap("redisKey");
-        hashmap.put("hashKey", "hashMap");
+        hashmap.put("hashKey_____11", "hashMap1");
+//        TimeUnit.SECONDS.sleep(10);
+        hashmap.put("hashKey_____22", "hashMap2");
+        hashmap.put("hashKey_____33", "hashMap3");
 
     }
 }
