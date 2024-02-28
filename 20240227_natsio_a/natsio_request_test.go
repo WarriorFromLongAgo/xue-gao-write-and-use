3d661c4a73a48b1fb69f70189bd11012d407ed25
xuegao <hubeixuegao@gmail.com>
2024-02-28 22:14:00 +0800

    go web3

--- 20240227_natsio_a/natsio_request_test.go
+++ 20240227_natsio_a/natsio_request_test.go
@@ -2,17 +2,22 @@ package _0240227_natsio_a
 
 import (
 	"github.com/nats-io/nats.go"
+	"log"
 	"testing"
 )
 
 func Test_natsio_request(t *testing.T) {
 	url := nats.DefaultURL
 
-	nc, _ := nats.Connect(url)
+	nc, err := nats.Connect(url)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 	defer nc.Drain()
 
 	// Matches all of the above
-	nc.Publish("foo.bar.baz", []byte("Hello World")) // Use the response
+	nc.Publish("foo", []byte("Hello World")) // Use the response
 
-	println("request request request")
+	println("====================  request request request")
 }
--- 20240227_natsio_a/natsio_resp_test.go
+++ 20240227_natsio_a/natsio_resp_test.go
@@ -3,16 +3,22 @@ package _0240227_natsio_a
 import (
 	"fmt"
 	"github.com/nats-io/nats.go"
+	"log"
 	"testing"
 )
 
 func Test_natsio_resp(t *testing.T) {
 	url := nats.DefaultURL
 
-	nc, _ := nats.Connect(url)
+	nc, err := nats.Connect(url)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 	defer nc.Drain()
 
-	nc.Subscribe("foo.bar.baz", func(m *nats.Msg) {
+	nc.Subscribe("foo", func(m *nats.Msg) {
 		fmt.Printf("Msg received on [%s] : %s\n", m.Subject, string(m.Data))
 	})
+	println("====================  resp ")
 }
